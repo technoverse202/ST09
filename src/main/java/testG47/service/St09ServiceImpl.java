6e6efd92e8988a9abc2e1375c553f434b7c85c99
Developer Platform <>
2024-01-23 08:28:42 +0000

    Init Upload

---
+++ src/main/java/testG47/service/St09ServiceImpl.java
@@ -0,0 +1,15 @@
+package testG47.service;
+
+import jakarta.enterprise.context.ApplicationScoped;
+ 
+   
+   
+   
+
+
+
+@ApplicationScoped
+public class St09ServiceImpl implements St09Service{
+   
+    
+}  
